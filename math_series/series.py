7b5fd8753f91bb7c8d1813b22ade7c0256ca0539
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-04-19 17:44:23 -0700

    complteted all test, added edge case -3

--- math_series/series.py
+++ math_series/series.py
@@ -1,22 +1,22 @@
 def Fibonacci(n):
-  
-  if n < 0:
-    print("Cannot compute")
-  elif n == 0:
-    return 0
-  elif n == 1:
-    return 1
-  elif n > 1:
-    return Fibonacci(n-1) + Fibonacci(n-2)
+
+    if n < 0:
+        print("Cannot compute")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n > 1:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
 
 def Lucas(n):
-  
-  if n < 0:
-    print("Cannot compute")
-  elif n == 0:
-    return 0
-  elif n == 1:
-    return 1
-  elif n > 1:
-    return Lucas(n-1) + Lucas(n-2)
-   
+
+    if n < 0:
+        print("Cannot compute")
+    elif n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    elif n > 1:
+        return Lucas(n-1) + Lucas(n-2)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,73 +1,100 @@
-from math_series.series import Fibonacci , Lucas
+from math_series.series import Fibonacci, Lucas
 import pytest
+
 
 def test_Lucas_zero():
     actual = Lucas(0)
     expected = 2
     assert actual == expected
-    
+
+
+
 def test_Lucas_one():
     actual = Lucas(1)
     expected = 1
     assert actual == expected
-    
+
+
+
 def test_Lucas_two():
     actual = Lucas(2)
     expected = 3
     assert actual == expected
-    
+
+
 def test_Lucas_three():
     actual = Lucas(3)
     expected = 4
     assert actual == expected
-    
+
+
 def test_Lucas_four():
     actual = Lucas(4)
     expected = 7
     assert actual == expected
-    
+
+
 def test_Lucas_five():
     actual = Lucas(5)
     expected = 11
     assert actual == expected
-    
+
+
 def test_Lucas_six():
     actual = Lucas(6)
     expected = 18
     assert actual == expected
-@pytest.mark.skip
+
+
 def test_Fibonacci_zero():
     actual = Fibonacci(0)
     expected = 0
     assert actual == expected
-@pytest.mark.skip   
+
+
 def test_Fibonacci_one():
     actual = Fibonacci(1)
     expected = 1
     assert actual == expected
-@pytest.mark.skip    
+
+
 def test_Fibonacci_two():
     actual = Fibonacci(2)
     expected = 1
     assert actual == expected
-@pytest.mark.skip    
+
+
 def test_Fibonacci_three():
     actual = Fibonacci(3)
     expected = 2
     assert actual == expected
-@pytest.mark.skip    
+
+
 def test_Fibonacci_four():
     actual = Fibonacci(4)
     expected = 3
     assert actual == expected
-@pytest.mark.skip    
+
+
 def test_Fibonacci_five():
     actual = Fibonacci(5)
     expected = 5
     assert actual == expected
-@pytest.mark.skip    
+
+
 def test_Fibonacci_six():
     actual = Fibonacci(6)
     expected = 8
     assert actual == expected
+
+# edge cases
+
+def test_Fibonacci_neg():
+    actual = Fibonacci(-3)
+    expected = "Cannot compute"
+    assert actual == expected
     
+def test_Lucas_neg():
+    actual = Lucas(-3)
+    expected = "Cannot compute"
+    assert actual == expected
